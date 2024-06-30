a165ce52636faf4b2529d8ce3733d61828edc41d
SMDX Development <emilsven2021@gmail.com>
2024-06-30 19:24:34 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -10,7 +10,7 @@ local function versionCheckPrint(_type, log)
 end
 
 local function CheckVersion()
-    PerformHttpRequest('', function(err, text, headers)
+    PerformHttpRequest('https://raw.githubusercontent.com/SMDX-RedM-Framework/smdx-wardrobe/main/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
         if not text then 
